a39a1807588f8cb5a83d2b1c9ee18d8605d3664d
Rholdan <roldanhollow@gmail.com>
2021-09-18 17:54:19 -0500

    N-API: Base proyecto, h2 y librerias

---
+++ numeros-api/src/main/java/com/appgate/numeros/NumerosApplication.java
@@ -0,0 +1,12 @@
+package com.appgate.numeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumerosApplication.class, args);
+    }
+}
