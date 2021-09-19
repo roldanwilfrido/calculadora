e0bc5ea8f60fc386909fe6f5ae7c4e3e6b1d58c0
Rholdan <roldanhollow@gmail.com>
2021-09-18 19:47:34 -0500

    C-API: Base proyecto y librerias

---
+++ calculos-api/src/main/java/com/appgate/calculos/CalculosApplication.java
@@ -0,0 +1,12 @@
+package com.appgate.calculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculosApplication.class, args);
+    }
+}
